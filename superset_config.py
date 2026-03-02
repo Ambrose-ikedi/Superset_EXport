b1d594b4313466d91b2a67e390c648c0b68cc4fa
Ambrosia Kene <ambroseikedi18@gmail.com>
2026-03-02 08:42:13 +0300

    Add superset config and update cache settings for Render deployment

--- superset_config.py
+++ superset_config.py
@@ -7,16 +7,25 @@ FEATURE_FLAGS = {
     "EMBEDDED_SUPERSET": True,
 }
 
+# Use in-memory cache if Redis not configured
 REDIS_HOST = os.getenv("REDIS_HOST_KEY")
 REDIS_PORT = os.getenv("REDIS_PORT_KEY", 6379)
 
-CACHE_CONFIG = {
-    "CACHE_TYPE": "RedisCache",
-    "CACHE_DEFAULT_TIMEOUT": 300,
-    "CACHE_REDIS_HOST": REDIS_HOST,
-    "CACHE_REDIS_PORT": REDIS_PORT,
-    "CACHE_REDIS_DB": 1,
-}
-
-CELERY_BROKER_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
-CELERY_RESULT_BACKEND = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
+if REDIS_HOST:
+    CACHE_CONFIG = {
+        "CACHE_TYPE": "RedisCache",
+        "CACHE_DEFAULT_TIMEOUT": 300,
+        "CACHE_REDIS_HOST": REDIS_HOST,
+        "CACHE_REDIS_PORT": REDIS_PORT,
+        "CACHE_REDIS_DB": 1,
+    }
+    CELERY_BROKER_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
+    CELERY_RESULT_BACKEND = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
+else:
+    # fallback to in-memory cache
+    CACHE_CONFIG = {
+        "CACHE_TYPE": "SimpleCache",
+        "CACHE_DEFAULT_TIMEOUT": 300,
+    }
+    CELERY_BROKER_URL = None
+    CELERY_RESULT_BACKEND = None
